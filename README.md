53b45c1df59259c4792ac5f4d82481ea2af3851f
GanescuRobert <studentGanescuRobertFlorinel@gmail.com>
2022-03-07 12:32:37 +0200

    index buffer

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # LearnOpenGL
 
-| Nr. | Video name                                                                                              |
-| --- | ------------------------------------------------------------------------------------------------------- |
-| 1.  | [Install](https://www.youtube.com/watch?v=XpBGwZNyUh0&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=1)  |
-| 2.  | [Window](https://www.youtube.com/watch?v=z03LXhRBLGI&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=2)   |
-| 3.  | [Triangle](https://www.youtube.com/watch?v=hYZNN0MTLuc&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=3) |
+| Nr. | Video name                                                                                                  |
+| --- | ----------------------------------------------------------------------------------------------------------- |
+| 1.  | [Install](https://www.youtube.com/watch?v=XpBGwZNyUh0&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=1)      |
+| 2.  | [Window](https://www.youtube.com/watch?v=z03LXhRBLGI&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=2)       |
+| 3.  | [Triangle](https://www.youtube.com/watch?v=hYZNN0MTLuc&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=3)     |
+| 4.  | [Index Buffer](https://www.youtube.com/watch?v=KG9ZXKaJWwY&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=4) |
 
 ## References
 
--- YoutubeOpenGL/Main.cpp
+++ YoutubeOpenGL/Main.cpp
@@ -29,9 +29,15 @@ int main() {
 	GLfloat vertices[] = {
 		-0.5f,-0.5f * float(sqrt(3)) / 3, 0.0f,
 		0.5f,-0.5f * float(sqrt(3)) / 3, 0.0f,
-		0.0f,0.5f * float(sqrt(3)) * 2 / 3, 0.0f
-
+		0.0f,0.5f * float(sqrt(3)) * 2 / 3, 0.0f,
+		-0.5f / 2,0.5f * float(sqrt(3)) / 6, 0.0f,
+		0.5f / 2,0.5f * float(sqrt(3)) / 6, 0.0f,
+		0.0f,-0.5f * float(sqrt(3)) / 3, 0.0f,
 	};
+	GLuint indices[] = {
+						0,3,5,
+						3,2,4,
+						5,4,1 };
 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
 	if (window == NULL) {
@@ -44,36 +50,42 @@ int main() {
 	gladLoadGL();
 	glViewport(0, 0, 800, 800);
 
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER); 
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); 
-	glCompileShader(vertexShader); 
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
 
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
-	GLuint shaderProgram = glCreateProgram(); 
+	GLuint shaderProgram = glCreateProgram();
 
 	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader); 
+	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 
 	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader); 
+	glDeleteShader(fragmentShader);
 
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	glClearColor(0.05f, 0.15f, 0.15f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
@@ -82,9 +94,10 @@ int main() {
 		glClearColor(0.05f, 0.15f, 0.15f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram); // Compile
+		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT,0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
@@ -92,6 +105,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window);
