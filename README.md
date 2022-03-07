b4869223f27bbf76ebe760e632919a5a1b333ef6
GanescuRobert <studentGanescuRobertFlorinel@gmail.com>
2022-03-07 11:26:22 +0200

    window

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 | Nr. | Video name                                                                                             |
 | --- | ------------------------------------------------------------------------------------------------------ |
 | 1.  | [Install](https://www.youtube.com/watch?v=XpBGwZNyUh0&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=1) |
+| 2.  | [Window](https://www.youtube.com/watch?v=z03LXhRBLGI&list=PLPaoO-vpZnumdcb4tZc4x5Q-v7CkrQ6M-&index=2)  |
--- YoutubeOpenGL/Main.cpp
+++ YoutubeOpenGL/Main.cpp
@@ -2,5 +2,34 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 int main() {
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, 800, 800);
+	glClearColor(0.05f, 0.15f, 0.15f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+
+
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
