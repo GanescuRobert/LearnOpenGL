13a021f71577d569ea53c77047605880344d4754
GanescuRobert <studentGanescuRobertFlorinel@gmail.com>
2022-03-11 10:31:23 +0200

    textures

--- YoutubeOpenGL/Main.cpp
+++ YoutubeOpenGL/Main.cpp
@@ -1,7 +1,12 @@
+#include<filesystem>
+namespace fs = std::filesystem;
+
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
+#include"Texture.h"
 #include "shaderClass.h"
 #include "VAO.h"
 #include "VBO.h"
@@ -16,18 +21,17 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLfloat vertices[] = {
-		/*	|				Coordinates								|		Colors			|	*/
-				-0.5f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.3f, 0.02f,
-				0.5f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.3f, 0.02f,
-				0.0f,		0.5f * float(sqrt(3)) * 2 / 3,	0.0f,		1.0f, 0.6f, 0.32f,
-				-0.5f / 2,	0.5f * float(sqrt(3)) / 6,		0.0f,		0.9f, 0.45f, 0.17f,
-				0.5f / 2,	0.5f * float(sqrt(3)) / 6,		0.0f,		0.9f, 0.45f, 0.17f,
-				0.0f,		-0.5f * float(sqrt(3)) / 3,		0.0f,		0.8f, 0.3f, 0.02f,
+		/*	|			Coordinates				|		Colors			|		Axes		| */
+				-0.5f,		-0.5f,		0.0f,		1.0f, 0.0f, 0.0f,		0.0f, 0.0f,
+				-0.5f,		 0.5f,		0.0f,		0.0f, 1.0f, 0.0f,		0.0f, 1.0f,
+				 0.5f,		 0.5f,		0.0f,		0.0f, 0.0f, 1.0f,		1.0f, 1.0f,
+				 0.5f,		-0.5f,		0.0f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+
 	};
 	GLuint indices[] = {
-						0,3,5,
-						3,2,4,
-						5,4,1 };
+						0,2,1,
+						0,3,2
+	};
 
 	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
 	if (window == NULL) {
@@ -48,13 +52,20 @@ int main() {
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3*sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	std::string parentDir = (fs::current_path(). fs::path::parent_path()).string();
+	std::string texPath = "/Resources/";
+
+	Texture popCat((parentDir + texPath + "pop_cat.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	popCat.texUnit(shaderProgram, "tex0", 0);
 
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.05f, 0.15f, 0.15f, 1.0f);
@@ -62,6 +73,8 @@ int main() {
 
 		shaderProgram.Activate();
 		glUniform1f(uniID, 0.5f);
+		popCat.Bind();
+
 		VAO1.Bind();
 
 		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
@@ -72,7 +85,8 @@ int main() {
 
 	VAO1.Delete();
 	VBO1.Delete();
-	EBO1.Delete();
+	EBO1.Delete(); 
+	popCat.Delete();
 	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);
